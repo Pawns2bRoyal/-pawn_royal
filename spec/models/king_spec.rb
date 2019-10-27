6a79933feb830c6f8ceed61ec14e9b1cff615631
Mark Adgerson <adgerson11@gmail.com>
2019-10-27 17:16:59 +0000

    Removd index.html from stylesheet, modified King_spec

--- spec/models/king_spec.rb
+++ spec/models/king_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe King, type: :model do
+    let(:game) { Game.new }
+    let(:board) { Board.new(true) }
+    let(:king) { King.new([4, 4], true) }
+    let(:enemy_king) { King.new([0, 0], false) }
+    before do
+        game.board = board
+        board.positions[0][0] = enemy_king
+        board.positions[4][4] = king
+    end
+
   
 end

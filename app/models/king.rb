9368f259ab53a0d4bc10746280d3d5e8d3595d0b
Mark Adgerson <adgerson11@gmail.com>
2019-11-12 19:25:36 +0000

    updated branch with master

--- app/models/king.rb
+++ app/models/king.rb
@@ -1,6 +1,5 @@
 class King < Piece
     
-<<<<<<< HEAD
     def valid_move?(new_x_coordinate, new_y_coordinate, id = nil, color = nil)
         x_distance = x_distance(new_x_coordinate)
         y_distance = y_distance(new_y_coordinate)
@@ -10,6 +9,4 @@ class King < Piece
         (y_distance == 1 && y_distance == x_distance) ||
         legal_to_castle?(new_x_coordinate, new_y_coordinate)
     end
-=======
->>>>>>> ee8c5d3c161ff331846101214d2a5613b4f7d8df
 end

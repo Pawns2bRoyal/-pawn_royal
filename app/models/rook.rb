d9f35900a930355213b3ef557695075bb876b62b
Mark Adgerson <adgerson11@gmail.com>
2019-11-20 19:23:08 +0000

    Added valid_move? for Rook

--- app/models/rook.rb
+++ app/models/rook.rb
@@ -1,2 +1,10 @@
 class Rook < Piece
+
+    def valid_move?(new_x_coordinate, new_y_coordinate, id = nil, color = nil)
+        x_distance = x_distance(new_x_coordinate)
+        y_distance = y_distance(new_y_coordinate)
+
+        (x_distance >= 1 && y_distance == 0) || 
+        (y_distance >= 1 && x_distance == 0)
+    end
 end
--- spec/models/rook_spec.rb
+++ spec/models/rook_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Rook, type: :model do
+
+        describe "#valid_move?" do
+        it "should return true to move one square forward" do 
+            game = Game.create
+            rook = FactoryBot.create :rook, x_coordinate: 5, y_coordinate: 5, game_id: game.game_id
+            expect(rook.valid_move?(5,6)).to eq(true)
+        end
+    end
+
   
 end
